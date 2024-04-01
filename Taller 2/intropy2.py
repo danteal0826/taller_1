91fd0cdd2fa54f205e79207f5ca1353ac8808892
Daniela Nieto <nietogonzalezdaniela@gmail.com>
2024-04-01 14:54:26 -0500

    ciclos

---
+++ Taller 2/intropy2.py
@@ -0,0 +1,13 @@
+# While
+# while<condicion_Verdadera>:
+#   cuerpo del ciclo
+#Condiciones son: expresiones booleanas (or, and) y operaciones de comparacion
+#Ciclos controlados por un contador enteros
+i=0
+
+while i<10:
+    print("ciclo")
+    #Importante modificar el valor del contador
+    i=i+1 #i+=1<-equivalente; i++ en python no existe
+
+# for
